7645d2c226a48628b6499a06eac2bdc56933bd3c
Max Pronko <max.pronko@gmail.com>
2020-12-16 10:57:26 +0200

    Added lesson 6 code

---
+++ lesson-6/index.php
@@ -0,0 +1,66 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Blog\PostMapper;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
+
+$config = include 'config/database.php';
+$dsn = $config['dsn'];
+$username = $config['username'];
+$password = $config['password'];
+
+try {
+    $connection = new PDO($dsn, $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $exception) {
+    echo 'Database error: ' . $exception->getMessage();
+    die();
+}
+
+$postMapper = new PostMapper($connection);
+
+// Create app
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) use ($view, $postMapper) {
+    $posts = $postMapper->getList('DESC');
+
+    $body = $view->render('index.twig', [
+        'posts' => $posts
+    ]);
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('about.twig', [
+        'name' => 'Max'
+    ]);
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $postMapper) {
+    $post = $postMapper->getByUrlKey((string) $args['url_key']);
+
+    if (empty($post)) {
+        $body = $view->render('not-found.twig');
+    } else {
+        $body = $view->render('post.twig', [
+            'post' => $post
+        ]);
+    }
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->run();
---
+++ lesson-6/src/PostMapper.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blog;
+
+use Exception;
+use PDO;
+
+class PostMapper
+{
+    /**
+     * @var PDO
+     */
+    private PDO $connection;
+
+    /**
+     * PostMapper constructor.
+     * @param PDO $connection
+     */
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * @param string $urlKey
+     * @return array|null
+     */
+    public function getByUrlKey(string $urlKey): ?array
+    {
+        $statement = $this->connection->prepare('SELECT * FROM post WHERE url_key = :url_key');
+        $statement->execute([
+            'url_key' => $urlKey
+        ]);
+
+        $result = $statement->fetchAll();
+
+        return array_shift($result);
+    }
+
+    public function getList(string $direction): ?array
+    {
+        if (!in_array($direction, ['DESC', 'ASC'])) {
+            throw new Exception('The direction is not supported.');
+        }
+        $statement = $this->connection->prepare('SELECT * FROM post ORDER BY published_date ' . $direction);
+
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
+}
